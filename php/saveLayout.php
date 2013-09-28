a930f82dfadb825fd39986f609040284b9c30037
cc <cc@book.stowers>
2013-09-28 13:15:06 +0100

    php

--- php/saveLayout.php
+++ php/saveLayout.php
@@ -1,17 +1,11 @@
 <?php
-
-include 'queries.php';
-$id = $_POST["id"];
-$layout = $_POST["layout"];
-$connectionString = "host= dbname= user= password=";
-$dbconn = pg_connect($connectionString)
-        or die('Could not connect: ' . pg_last_error());
-
-$query = "UPDATE upload SET layout = '" . $layout . "' WHERE rand = '" . $id . "';";
-
-//echo $query;
-
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
-echo 'Layout saved';
-
+	include('../../uploadsConnectionString.php');
+	$id = $_POST["id"];
+	$layout = $_POST["layout"];
+	$dbconn = pg_connect($connectionString)
+			or die('Could not connect: ' . pg_last_error());
+	$query = "UPDATE upload SET layout = '" . $layout . "' WHERE rand = '" . $id . "';";
+	//echo $query;
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	echo 'Layout saved';
 ?>
