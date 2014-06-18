0f0d2c673d9cd2d76a69f0fb4ed15d5bfd1b0ad7
colin <colin@sce-bio-c01323.ed.ac.uk>
2014-06-18 12:17:45 +0100

    bug fix

--- js/model/Protein.js
+++ js/model/Protein.js
@@ -1025,33 +1025,27 @@ Protein.prototype.getResidueLinkPath = function(residueLink) {
 		
 		var angle;
 		if (residueLink.intraMolecular === true){
-		
 			var curveMidX = x1 + ((x2 - x1) / 2);
 			arcStart = [ curveMidX, height - arcRadius];
 			arcEnd =  [ curveMidX, height - arcRadius];
 			cp1 = [ curveMidX, height - arcRadius];
 			cp2 =  [ curveMidX, height - arcRadius];
-			flip();
+			//flip
+			start[1] = start[1] * -1;
+			cp1[1] = cp1[1] * -1;
+			arcStart[1] = arcStart[1] * -1;
+			arcEnd[1] = arcEnd[1] * -1;
+			cp2[1] = cp2[1] * -1;
+			end[1] = end[1] * -1;
 		}
 		else if (residueLink.hd){	
 			var curveMidX = x1 + ((x2 - x1) / 2);
-			//~ arcStart = Protein.rotatePointAboutPoint([curveMidX, height - arcRadius], start, -4);
-			//~ arcEnd = Protein.rotatePointAboutPoint([curveMidX, height - arcRadius], end, 4);
-			
 			arcStart = [curveMidX, height - arcRadius];
 			arcEnd = [curveMidX, height - arcRadius];
-			 
 			cp1 = Protein.rotatePointAboutPoint([x1, height - arcRadius], start, -20);
 			cp2 = Protein.rotatePointAboutPoint([x2, height - arcRadius], end, 20);
-			//~ flip();
-			
-			//var intersect = checkLineIntersection(line1StartX, line1StartY, line1EndX, line1EndY, line2StartX, line2StartY, line2EndX, line2EndY;
-			//~ var intersect = checkLineIntersection(cp1[0], cp1[1], start[0], start[1], 
-						//~ cp2[0], cp2[1], end[0], end[1]);			
-			//~ arcRadius = height - arcRadius - intersect.y;		
 		}
 		else {	
-			//~ angle = 0;
 			cp1 = [x1, height];
 			cp2 = [x2, baseLine];
 			arcStart = [x1, height];
@@ -1062,57 +1056,8 @@ Protein.prototype.getResidueLinkPath = function(residueLink) {
 			+ " Q "  + cp1[0] + ',' + cp1[1] + ' ' + arcStart[0] + "," + arcStart[1]
 			+ " A " + arcRadius + "," + arcRadius + "  0 0 1 " + arcEnd[0]  + "," + arcEnd[1]
 			+ " Q "+ cp2[0] + ',' + cp2[1] +  " "  + end[0] + "," + end[1];
-		
-		function flip(){
-			start[1] = start[1] * -1;
-			cp1[1] = cp1[1] * -1;
-			arcStart[1] = arcStart[1] * -1;
-			arcEnd[1] = arcEnd[1] * -1;
-			cp2[1] = cp2[1] * -1;
-			end[1] = end[1] * -1;
-		}
 	}
 }
-
-//~ function checkLineIntersection(line1StartX, line1StartY, line1EndX, line1EndY, line2StartX, line2StartY, line2EndX, line2EndY) {
-    //~ // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
-    //~ var denominator, a, b, numerator1, numerator2, result = {
-        //~ x: null,
-        //~ y: null,
-        //~ onLine1: false,
-        //~ onLine2: false
-    //~ };
-    //~ denominator = ((line2EndY - line2StartY) * (line1EndX - line1StartX)) - ((line2EndX - line2StartX) * (line1EndY - line1StartY));
-    //~ if (denominator == 0) {
-        //~ return result;
-    //~ }
-    //~ a = line1StartY - line2StartY;
-    //~ b = line1StartX - line2StartX;
-    //~ numerator1 = ((line2EndX - line2StartX) * a) - ((line2EndY - line2StartY) * b);
-    //~ numerator2 = ((line1EndX - line1StartX) * a) - ((line1EndY - line1StartY) * b);
-    //~ a = numerator1 / denominator;
-    //~ b = numerator2 / denominator;
-//~ 
-    //~ // if we cast these lines infinitely in both directions, they intersect here:
-    //~ result.x = line1StartX + (a * (line1EndX - line1StartX));
-    //~ result.y = line1StartY + (a * (line1EndY - line1StartY));
-//~ /*
-        //~ // it is worth noting that this should be the same as:
-        //~ x = line2StartX + (b * (line2EndX - line2StartX));
-        //~ y = line2StartX + (b * (line2EndY - line2StartY));
-        //~ */
-    //~ // if line1 is a segment and line2 is infinite, they intersect if:
-    //~ if (a > 0 && a < 1) {
-        //~ result.onLine1 = true;
-    //~ }
-    //~ // if line2 is a segment and line1 is infinite, they intersect if:
-    //~ if (b > 0 && b < 1) {
-        //~ result.onLine2 = true;
-    //~ }
-    //~ // if line1 and line2 are segments, they intersect if both of the above are true
-    //~ return result;
-//~ };
-
 
 
 Protein.prototype.showPeptides = function(pepBounds) {
@@ -1136,16 +1081,13 @@ Protein.prototype.showPeptides = function(pepBounds) {
 				annotColouredRect.setAttribute("y", y);
 				annotColouredRect.setAttribute("width", annoLength);
 				annotColouredRect.setAttribute("height", yIncrement);
-
 				//style 'em
 				annotColouredRect.setAttribute("fill", xiNET.highlightColour.toRGB());
 				//annotColouredRect.setAttribute("fill-opacity", "0.7");
-				
 				this.peptides.appendChild(annotColouredRect);
 			}
 			
 			if (pep[2]){//homodimer like
-				//~ alert("yup");
 				//TODO: eliminate duplication
 				annotColouredRect = document.createElementNS(xiNET.svgns, "rect");
 				annotColouredRect.setAttribute("class", "protein");
@@ -1160,8 +1102,8 @@ Protein.prototype.showPeptides = function(pepBounds) {
 				annotColouredRect.setAttribute("fill", xiNET.homodimerLinkColour.toRGB());
 				annotColouredRect.setAttribute("fill-opacity", "0.5");
 				
-				this.peptides.appendChild(annotColouredRect);		}
-			
+				this.peptides.appendChild(annotColouredRect);		
+			}
             y += yIncrement;
         }
    }		
