f85b4755fc9442e90875cbffce96e172a7ac5b5a
Joshua Heimbach <joshkh@gmail.com>
2014-08-06 17:31:44 +0100

    Converted NaryLink link backgrounds to paths to consume less screen space.

--- js/model/link/NaryLink.js
+++ js/model/link/NaryLink.js
@@ -1,10 +1,10 @@
-//    	xiNET interaction viewer
-//    	Copyright 2014 Rappsilber Laboratory
+//      xiNET interaction viewer
+//      Copyright 2014 Rappsilber Laboratory
 //
-//    	This product includes software developed at
-//    	the Rappsilber Laboratory (http://www.rappsilberlab.org/).
+//      This product includes software developed at
+//      the Rappsilber Laboratory (http://www.rappsilberlab.org/).
 //
-//		author: Colin Combe
+//      author: Colin Combe
 
 "use strict";
 
@@ -31,73 +31,76 @@ function NaryLink(id, xlvController) {
 }
 
 NaryLink.prototype.addEvidence = function(interaction) {
-	if (this.evidences.has(interaction.id) === false) {
-		this.evidences.set(interaction.id, interaction);
+    if (this.evidences.has(interaction.id) === false) {
+        this.evidences.set(interaction.id, interaction);
     
-		//~ if (this.evidences.values().length > NaryLink.maxNoEvidences) {//TODO: update d3 lib
-			//~ xiNET.Link.maxNoEvidences = this.evidences.values().length; //values().length can be replaced with size() in newer d3 lib
-		//~ }
-			
-		if (this.interactors === null){
-			this.initInteractors(interaction);
-		}    
-			
-		for (var pi = 0; pi < interaction.participants.length; pi++){
-			var sourceID = interaction.participants[pi].interactorRef;
-			var sourceInteractor = this.ctrl.interactors.get(sourceID);
-					
-			var bindingSites = interaction.participants[pi].bindingSites;
-			if (bindingSites){
-				var bsCount = bindingSites.length;
-				for (var bsi = 0; bsi < bsCount; bsi++){
+        //~ if (this.evidences.values().length > NaryLink.maxNoEvidences) {//TODO: update d3 lib
+            //~ xiNET.Link.maxNoEvidences = this.evidences.values().length; //values().length can be replaced with size() in newer d3 lib
+        //~ }
+            
+        if (this.interactors === null){
+            this.initInteractors(interaction);
+        }    
+            
+        for (var pi = 0; pi < interaction.participants.length; pi++){
+            var sourceID = interaction.participants[pi].interactorRef;
+            var sourceInteractor = this.ctrl.interactors.get(sourceID);
+                    
+            var bindingSites = interaction.participants[pi].bindingSites;
+            if (bindingSites){
+                var bsCount = bindingSites.length;
+                for (var bsi = 0; bsi < bsCount; bsi++){
 
-					var bindingSite = bindingSites[bsi];
-					if (bindingSite.linkedFeatures){
-						for (var fi = 0; fi < bindingSite.linkedFeatures.length; fi++){									
-							var target = this.ctrl.features.get(bindingSite.linkedFeatures[fi]); 
-							var targetInteractor = this.ctrl.interactors.get(target.interactor);
-							var linkID, fromInteractor, toInteractor;	
-							// these links are undirected and should have same ID regardless of which way round 
-							// source and target are
-							if (sourceID - 0 < target.interactor - 0) {
-								linkID = sourceID + '-' + target.interactor;
-								fromInteractor = sourceInteractor;
-								toInteractor = targetInteractor; 
-							} else {
-								linkID = target.interactor + '-' + sourceID;
-								fromInteractor = targetInteractor;
-								toInteractor = sourceInteractor; 
-							}
-							
-						}
-											
-						var link = this.ctrl.links.get(linkID);
-						if (typeof link === 'undefined') {
-							if (fromInteractor === toInteractor){
-								link = new UnaryLink(linkID, this.ctrl);
-								fromInteractor.addLink(link);
-							}else {
-								link = new BinaryLink(linkID, this.ctrl, fromInteractor,toInteractor);
-								fromInteractor.addLink(link);
-								toInteractor.addLink(link);
-							}
-							this.ctrl.links.set(linkID, link);
-						}
-						this.subLinks.set(linkID, link);
-						link.addEvidence(interaction);
-					}
-				}
-			}
-		}			
-	}
+                    var bindingSite = bindingSites[bsi];
+                    if (bindingSite.linkedFeatures){
+                        for (var fi = 0; fi < bindingSite.linkedFeatures.length; fi++){                                 
+                            var target = this.ctrl.features.get(bindingSite.linkedFeatures[fi]); 
+                            var targetInteractor = this.ctrl.interactors.get(target.interactor);
+                            var linkID, fromInteractor, toInteractor;   
+                            // these links are undirected and should have same ID regardless of which way round 
+                            // source and target are
+                            if (sourceID - 0 < target.interactor - 0) {
+                                linkID = sourceID + '-' + target.interactor;
+                                fromInteractor = sourceInteractor;
+                                toInteractor = targetInteractor; 
+                            } else {
+                                linkID = target.interactor + '-' + sourceID;
+                                fromInteractor = targetInteractor;
+                                toInteractor = sourceInteractor; 
+                            }
+                            
+                        }
+                                            
+                        var link = this.ctrl.links.get(linkID);
+                        if (typeof link === 'undefined') {
+                            if (fromInteractor === toInteractor){
+                                link = new UnaryLink(linkID, this.ctrl);
+                                fromInteractor.addLink(link);
+                            }else {
+                                link = new BinaryLink(linkID, this.ctrl, fromInteractor,toInteractor);
+                                fromInteractor.addLink(link);
+                                toInteractor.addLink(link);
+                            }
+                            this.ctrl.links.set(linkID, link);
+                        }
+                        this.subLinks.set(linkID, link);
+                        link.addEvidence(interaction);
+                    }
+                }
+            }
+        }           
+    }
 };
 
 NaryLink.prototype.initSVG = function() {
-	this.rect = document.createElementNS(xiNET.svgns, "rect");
-	this.rect.setAttribute('fill', NaryLink.naryColours(this.id));
-	this.rect.setAttribute('opacity', 0.4);
-	this.rect.setAttribute('rx', '30');
-	this.rect.setAttribute('ry', '30');
+
+    this.rect = document.createElementNS(xiNET.svgns, "path");
+    this.rect.setAttribute('fill', NaryLink.naryColours(this.id));
+    this.rect.setAttribute('opacity', 0.4);
+    this.rect.setAttribute('stroke', NaryLink.naryColours(this.id));
+    this.rect.setAttribute('stroke-linejoin', 'round');
+    this.rect.setAttribute('stroke-width', 40);
+
     //set the events for it
     var self = this;
     this.rect.onmousedown = function(evt) {
@@ -105,24 +108,26 @@ NaryLink.prototype.initSVG = function() {
     };
     this.rect.onmouseover = function(evt) {
         self.mouseOver(evt);
+        console.log("interactors", this.ctrl);
     };
     this.rect.onmouseout = function(evt) {
         self.mouseOut(evt);
     };
     this.rect.ontouchstart = function(evt) {
+        console.log("touchstart");
         self.touchStart(evt);
     };
 };
 
 NaryLink.prototype.showHighlight = function(show) {
-	//~ if (this.shown) {
-		//~ //we will iterate through all interactors and sublinks and highlight them
-		//~ this.highlightInteractors(show);
-		//~ var subLinks = this.subLinks.values();
-		//~ for (var s = 0; s < subLinks.length; s++) {
-			//~ subLinks[s].showHighlight(show);
-		//~ }
-	//~ }
+    //~ if (this.shown) {
+        //~ //we will iterate through all interactors and sublinks and highlight them
+        //~ this.highlightInteractors(show);
+        //~ var subLinks = this.subLinks.values();
+        //~ for (var s = 0; s < subLinks.length; s++) {
+            //~ subLinks[s].showHighlight(show);
+        //~ }
+    //~ }
 };
 
 
@@ -146,7 +151,7 @@ NaryLink.prototype.showHighlight = function(show) {
 
 NaryLink.prototype.check = function() {
 
-	
+    
     //~ var seqLinks = this.sequenceLinks.values();
     //~ var seqLinkCount = seqLinks.length;
     // if either end of interaction is 'parked', i.e. greyed out,
@@ -245,53 +250,54 @@ NaryLink.prototype.show = function() {
             if (typeof this.rect === 'undefined') {
                 this.initSVG();
             }
-			this.rect.setAttribute("stroke-width", this.ctrl.z * 1);
-			this.setLinkCoordinates();
-			this.ctrl.naryLinks.appendChild(this.rect);
-		}
+            // this.rect.setAttribute("stroke-width", this.ctrl.z * 1);
+            this.setLinkCoordinates();
+            this.ctrl.naryLinks.appendChild(this.rect);
+        }
     }
 };
 
 NaryLink.prototype.hide = function() {
     //~ if (this.shown) {
         //~ this.shown = false;
-		//~ if (this.thickLineShown) {
-			//~ this.ctrl.p_pLinksWide.removeChild(this.thickLine);
-		//~ }
-		//this.ctrl.highlights.removeChild(this.highlightLine);
-		//~ this.ctrl.p_pLinks.removeChild(this.rect);
+        //~ if (this.thickLineShown) {
+            //~ this.ctrl.p_pLinksWide.removeChild(this.thickLine);
+        //~ }
+        //this.ctrl.highlights.removeChild(this.highlightLine);
+        //~ this.ctrl.p_pLinks.removeChild(this.rect);
     //~ }
 };
 
+
+// Uses d3.geom.hull to calculate a path's move-to points based on an array of vertices. 
+var hullPath = function(values) {
+    // d3.geom.hull fails when values aren't perfect ([0,0] vertices, or less than three nodes)
+    // TODO: Figure out a better way than try catching!
+    var returnval = null;
+
+    try {
+        var calced = d3.geom.hull(values);
+        returnval = "M" + calced.join("L") + "Z";
+    } catch(err) {
+        return null
+    }
+
+    return returnval;
+};
+
 NaryLink.prototype.setLinkCoordinates = function(interactor) {
+
     if (this.shown) {//don't waste time changing DOM if link not visible
-		var northerly = null, southerly = null, 
-			westerly = null, easterly = null; //bounding interactors
-		var interactors = this.interactors;
-		var iCount = interactors.length;
-		for (var i = 0; i < iCount; i++){
-			var interactor = interactors[i];
-				
-			if (westerly === null || interactor.x < westerly.x) {
-				westerly = interactor;
-			}
-			
-			if (easterly === null || interactor.x > easterly.x) {
-				easterly = interactor;
-			}
-			
-			if (southerly === null || interactor.y > southerly.y) {
-				southerly = interactor;
-			}
-			
-			if (northerly === null || interactor.y < northerly.y) {
-				northerly = interactor;
-			}
-		}		
-		//~ console.debug(this.id + "\t" + this.interactors.length)
-		this.rect.setAttribute('x',westerly.x - 20);
-		this.rect.setAttribute('y',northerly.y - 20);
-		this.rect.setAttribute('width',(easterly.x - westerly.x) + 40);
-		this.rect.setAttribute('height',(southerly.y - northerly.y) + 40);
+        var northerly = null, southerly = null, 
+            westerly = null, easterly = null; //bounding interactors
+
+        var interactors = this.interactors;
+
+        var mapped = interactors.map(function(i) {
+            return [i.x, i.y];
+        });
+
+        var hullValues = hullPath(mapped);
+        this.rect.setAttribute('d',hullValues);
     }
 };
