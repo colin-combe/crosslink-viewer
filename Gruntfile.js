362f04a34d24a889d4386ca0916ccb6819836005
Joshua Heimbach <joshkh@gmail.com>
2014-09-05 15:58:30 +0100

    Updated bundleOptions to browserifyOptions.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = taskManager = function(grunt) {
         src: ['src/controller/Init.js'],
         dest: 'build/<%= config.bower.name %>-<%= config.bower.version %>.js',
         options: {
-          bundleOptions: {
+          browserifyOptions: {
             standalone: 'xiNET',
             debug: true
           }
